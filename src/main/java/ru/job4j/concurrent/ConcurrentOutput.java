2fd2653f2c00a6bcc97fbd2f5e538a46c3e41f58
Zhitenev <oleg@openres.ru>
2022-01-10 16:29:52 +0700

    1. Запуск нити. Thread#start() [#1016]

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,20 @@
+package ru.job4j.concurrent;
+
+/**
+ * Проверка вывода имени нитей и их последовательности.
+ */
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+
+        );
+        another.start();
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
