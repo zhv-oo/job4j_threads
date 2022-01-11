72bab6cdee73ea023df4d4d8cdd483e069e7b979
zhv-oo <zhitenev.oleg@yandex.ru>
2022-01-12 01:56:10 +0700

    3. Прерывание нити [#1019 #249512]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -10,22 +10,19 @@ public class ConsoleProgress implements Runnable {
     @Override
     public void run() {
         List<Character> process = List.of('\\', '|', '/');
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        int i = 0;
-                        while (!Thread.currentThread().isInterrupted()) {
-                            Thread.sleep(500);
-                            System.out.print("\r load: " + process.get(i++));
-                            if (i == 3) {
-                                i = 0;
-                            }
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                });
-        thread.start();
+        int i = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r load: " + process.get(i++));
+            if (i == process.size()) {
+                i = 0;
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     public static void main(String[] args) throws InterruptedException {
