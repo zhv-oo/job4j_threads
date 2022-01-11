04b1346b2408720a6e9829c2f3e52ef6e784d587
Zhitenev <oleg@openres.ru>
2022-01-11 23:06:47 +0700

    3. Прерывание нити [#1019]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -12,17 +12,17 @@ public class ConsoleProgress implements Runnable {
         List<Character> process = List.of('\\', '|', '/');
         Thread thread = new Thread(
                 () -> {
-                    int i = 0;
-                    while (!Thread.currentThread().isInterrupted()) {
-                        System.out.print("\r load: " + process.get(i++));
-                        if (i == 3) {
-                            i = 0;
-                        }
-                        try {
+                    try {
+                        int i = 0;
+                        while (!Thread.currentThread().isInterrupted()) {
                             Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            System.out.print("\r load: " + process.get(i++));
+                            if (i == 3) {
+                                i = 0;
+                            }
                         }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
                 });
         thread.start();
