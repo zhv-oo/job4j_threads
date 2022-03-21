500b277ccab8ffa2bccd1e11d49737fd23007aa8
zhv-oo <zhitenev.oleg@yandex.ru>
2022-03-21 21:19:14 +0700

    0. Что такое атомарность? [#6856]

---
+++ src/main/java/ru/job4j/Count.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Count {
+    private int value;
+
+    public void increment() {
+        value++;
+    }
+
+    public int get() {
+        return value;
+    }
+}
