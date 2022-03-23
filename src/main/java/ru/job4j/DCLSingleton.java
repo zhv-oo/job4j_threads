776f59fafad40c170bf20fbd24f2700df6f8ae44
zhv-oo <zhitenev.oleg@yandex.ru>
2022-03-23 21:09:36 +0700

    2. Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/DCLSingleton.java
@@ -0,0 +1,23 @@
+package ru.job4j;
+
+/**
+ * Класс для реализации Double Checked Locking & volatile.
+ */
+public final class DCLSingleton {
+
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+    private DCLSingleton() {
+    }
+}
---
+++ src/main/java/ru/job4j/Flag.java
@@ -0,0 +1,28 @@
+package ru.job4j;
+
+/**
+ * Класс для работы с volatile.
+ */
+
+public class Flag {
+    private static volatile boolean flag = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(
+                () -> {
+                    while (flag) {
+                        System.out.println(Thread.currentThread().getName());
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        thread.start();
+        Thread.sleep(1000);
+        flag = false;
+        thread.join();
+    }
+}
