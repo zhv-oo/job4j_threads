594b31d69c7827b677eb84e98e5c3609d06b0cd7
zhv-oo <zhitenev.oleg@yandex.ru>
2022-04-04 08:03:42 +0700

    2. JCIP. Настройка библиотеки [#268575]

--- src/main/java/ru/job4j/Count.java
+++ src/main/java/ru/job4j/Count.java
@@ -1,13 +1,18 @@
 package ru.job4j;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
 public class Count {
+    @GuardedBy("this")
     private int value;
 
-    public void increment() {
-        value++;
+    public synchronized void increment() {
+        this.value++;
     }
 
-    public int get() {
-        return value;
+    public synchronized int get() {
+        return this.value;
     }
 }
---
+++ src/test/java/ru/job4j/CountTest.java
@@ -0,0 +1,35 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CountTest {
+    /**
+     * Класс описывает нить со счетчиком.
+     */
+    private class ThreadCount extends Thread {
+        private final Count count;
+
+        private ThreadCount(final Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        final Count count = new Count();
+        Thread first = new ThreadCount(count);
+        Thread second = new ThreadCount(count);
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        assertThat(count.get(), is(2));
+    }
+}
