61d6999365ea793213635f344328d00ca0691a90
zhv-oo <zhitenev.oleg@yandex.ru>
2022-04-05 09:13:59 +0700

    0. Управление нитью через wait. [#6858]

---
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -0,0 +1,31 @@
+package ru.job4j;
+
+/**
+ * Управление нитью через wait. Через счетчик.
+ */
+public class CountBarrier {
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public synchronized void count() {
+            count++;
+            monitor.notifyAll();
+    }
+
+    public synchronized void await() {
+        while (count >= total) {
+            try {
+                monitor.wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
